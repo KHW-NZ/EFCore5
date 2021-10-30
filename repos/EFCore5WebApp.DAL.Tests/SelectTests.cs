49cccc884370189156e119e6326fd976263d9e1a
Hyunwoo Kim <devguru.kim@gmail.com>
2021-10-30 22:00:29 +1300

    Add NUnit Insert tests

--- repos/EFCore5WebApp.DAL.Tests/SelectTests.cs
+++ repos/EFCore5WebApp.DAL.Tests/SelectTests.cs
@@ -1,4 +1,7 @@
+using System.Linq;
 using NUnit.Framework;
+using System.Collections.Generic;
+using EFCore5WebApp.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCore5WebApp.DAL.Tests
@@ -12,13 +15,33 @@ namespace EFCore5WebApp.DAL.Tests
         [SetUp]
         public void Setup()
         {
-            _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(Microsoft.Extensions.Configuration.ConfigurationExtensions.GetConnectionString(this._configuration, "connection"));
+            _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>().UseSqlServer("Data Source=DESKTOP-M35SRPF;Initial Catalog=EFCore5App;Integrated Security=True;").Options);
         }
 
         [Test]
-        public void Test1()
+        public void GetAllPersons()
         {
-            Assert.Pass();
+            IEnumerable<Person> persons = _context.Persons.ToList();
+            Assert.AreEqual(2, persons.Count());
+        }
+
+        [Test]
+        public void PersonsHaveTwoAddresses()
+        {
+            List<Person> persons = _context.Persons.Include("Addresses").ToList();
+            Assert.AreEqual(1, persons[0].Addresses.Count);
+            Assert.AreEqual(2, persons[1].Addresses.Count);
+        }
+
+        [Test]
+        public void HaveLookUpRecords()
+        {
+            var lookUps = _context.LookUps.ToList();
+            var countries = lookUps.Where(x => x.LookUpType == LookUpType.Country).ToList();
+            var states = lookUps.Where(x => x.LookUpType == LookUpType.State).ToList();
+
+            Assert.AreEqual(1, countries.Count);
+            Assert.AreEqual(51, states.Count);
         }
     }
 }
